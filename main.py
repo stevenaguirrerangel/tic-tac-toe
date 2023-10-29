67a0f8454abfa1b5fd22bbb794f3997a6bfa9ab0
Steven <steven.aguirrerangel@gmail.com>
2023-10-29 11:07:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+BOARD = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+CURRENT_PLAYER = "X"
+winner = None
+game_running = True
+
+
+# printing the game board
+def print_board(board):
+    print(f" {board[0]}  |  {board[1]}  |  {board[2]}")
+    print("---------------")
+    print(f" {board[3]}  |  {board[4]}  |  {board[5]}")
+    print("---------------")
+    print(f" {board[6]}  |  {board[7]}  |  {board[8]}")
+
+# take player input
+
+
+# check for win or tie
+
+
+# switch the player
+
+
+# check for the win or tie again
