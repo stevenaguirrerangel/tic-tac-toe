4ccc185d098f45583fafaa7b9d86321d403736ec
Steven <steven.aguirrerangel@gmail.com>
2023-10-29 12:21:02 -0500

    Complete game functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-BOARD = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
-
+BOARD = ["1", "2", "3",
+         "4", "5", "6",
+         "7", "8", "9"]
+board_start = ["1", "2", "3",
+               "4", "5", "6",
+               "7", "8", "9"]
 CURRENT_PLAYER = "X"
-winner = None
-game_running = True
+WINNER = None
+GAME_RUNNING = True
 
 
 # printing the game board
@@ -15,13 +17,86 @@ def print_board(board):
     print("---------------")
     print(f" {board[6]}  |  {board[7]}  |  {board[8]}")
 
+
 # take player input
+def player_input(board):
+    choice = input("Enter a number 1-9: ")
+    choice_num = int(choice)
+    if 1 <= choice_num <= 9 and board[choice_num - 1] == choice:
+        board[choice_num - 1] = CURRENT_PLAYER
+    else:
+        print("This spot is already in play")
 
 
 # check for win or tie
+def check_horizontal(board):
+    global WINNER
+    if board[0] == board[1] == board[2]:
+        WINNER = board[0]
+        return True
+    elif board[3] == board[4] == board[5]:
+        WINNER = board[3]
+        return True
+    elif board[6] == board[7] == board[8]:
+        WINNER = board[6]
+        return True
 
 
-# switch the player
+def check_vertical(board):
+    global WINNER
+    if board[0] == board[3] == board[6]:
+        WINNER = board[0]
+        return True
+    elif board[1] == board[4] == board[7]:
+        WINNER = board[1]
+        return True
+    elif board[2] == board[5] == board[8]:
+        WINNER = board[2]
+        return True
+
+
+def check_diagonal(board):
+    global WINNER
+    if board[0] == board[4] == board[8]:
+        WINNER = board[0]
+        return True
+    elif board[6] == board[4] == board[2]:
+        WINNER = board[2]
+        return True
+
+
+def check_tie(board):
+    global GAME_RUNNING
+    no_tie = any((True for x in board_start if x in board))
+    if no_tie:
+        pass
+    else:
+        print_board(board)
+        print("It is a tie!")
+        GAME_RUNNING = False
 
 
 # check for the win or tie again
+def check_win(board):
+    global GAME_RUNNING
+    if check_diagonal(board) or check_horizontal(board) or check_vertical(board):
+        print_board(BOARD)
+        GAME_RUNNING = False
+        print(f"The winner is {WINNER}")
+
+
+# switch the player
+def player_switch():
+    global CURRENT_PLAYER
+    if CURRENT_PLAYER == "X":
+        CURRENT_PLAYER = "O"
+    else:
+        CURRENT_PLAYER = "X"
+
+
+while GAME_RUNNING:
+    print_board(BOARD)
+    player_input(BOARD)
+    check_win(BOARD)
+    check_tie(BOARD)
+    player_switch()
